aa508dfcf38bd01e34ccca416dda2a821a6d9fcf
SauraSilva <saura.silva@gmail.com>
2017-05-17 09:23:03 -0300

    Quality control Pre Processing
    Script to run FASTQC to generate a report about the status of reads pre process.

---
+++ Scripts/fastqc_pre.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+fastqc ../../raw2/Uame-Purple_B1_1.fastq ../../raw2/Uame-Purple_B1_2.fastq -f fastq -o /work/projects/flowers/FastQC/pre
+fastqc ../../raw2/Uame-Purple_B2_1.fastq ../../raw2/Uame-Purple_B2_2.fastq -f fastq -o /work/projects/flowers/FastQC/pre
+fastqc ../../raw2/Uame-Purple_B3_1.fastq ../../raw2/Uame-Purple_B3_2.fastq -f fastq -o /work/projects/flowers/FastQC/pre
+
+fastqc ../../raw2/Uame-White_B1_1.fastq ../../raw2/Uame-White_B1_2.fastq -f fastq -o /work/projects/flowers/FastQC/pre
+fastqc ../../raw2/Uame-White_B2_1.fastq ../../raw2/Uame-White_B2_2.fastq -f fastq -o /work/projects/flowers/FastQC/pre
+fastqc ../../raw2/Uame-White_B3_1.fastq ../../raw2/Uame-White_B3_2.fastq -f fastq -o /work/projects/flowers/FastQC/pre
+
+fastqc ../../raw2/Uame-Yellow_B1_1.fastq ../../raw2/Uame-Yellow_B1_2.fastq -f fastq -o /work/projects/flowers/FastQC/pre
+fastqc ../../raw2/Uame-Yellow_B2_1.fastq ../../raw2/Uame-Yellow_B2_2.fastq -f fastq -o /work/projects/flowers/FastQC/pre
+
+fastqc ../../raw2/Utri-Yellow_B1_1.fastq ../../raw2/Utri-Yellow_B1_2.fastq -f fastq -o /work/projects/flowers/FastQC/pre
+
