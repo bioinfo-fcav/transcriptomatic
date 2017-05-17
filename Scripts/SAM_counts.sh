64e9ecd7feb305c0254ea262b4fcfc27d5a88671
SauraSilva <saura.silva@gmail.com>
2017-05-17 11:33:40 -0300

    Counting of mapped reads
    Counting of mapped reads and counting of best mapped reads (Q10)

---
+++ Scripts/SAM_counts.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+#samtools sort -n detonate_STM/Uame-Purple_detonate_B1.bam detonate_STM/Uame-Purple_detonate_B1.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-Purple_detonate_B1.bam.sorted.bam
+
+#samtools sort -n detonate_STM/Uame-Purple_detonate_B2.bam detonate_STM/Uame-Purple_detonate_B2.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-Purple_detonate_B2.bam.sorted.bam
+
+#samtools sort -n detonate_STM/Uame-Purple_detonate_B3.bam detonate_STM/Uame-Purple_detonate_B3.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-Purple_detonate_B3.bam.sorted.bam
+
+#samtools sort -n detonate_STM/Uame-White_detonate_B1.bam detonate_STM/Uame-White_detonate_B1.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-White_detonate_B1.bam.sorted.bam
+
+#samtools sort -n detonate_STM/Uame-White_detonate_B2.bam detonate_STM/Uame-White_detonate_B2.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-White_detonate_B2.bam.sorted.bam
+
+#samtools sort -n detonate_STM/Uame-White_detonate_B3.bam detonate_STM/Uame-White_detonate_B3.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-White_detonate_B3.bam.sorted.bam
+
+#samtools sort -n detonate_STM/Uame-Yellow_detonate_B1.bam detonate_STM/Uame-Yellow_detonate_B1.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-Yellow_detonate_B1.bam.sorted.bam
+
+#samtools sort -n detonate_STM/Uame-Yellow_detonate_B2.bam detonate_STM/Uame-Yellow_detonate_B2.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-Yellow_detonate_B2.bam.sorted.bam
+
+#samtools sort -n detonate_STM/Utri-Yellow_detonate_B1.bam detonate_STM/Utri-Yellow_detonate_B1.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Utri-Yellow_detonate_B1.bam.sorted.bam
+
+#samtools view -q 10 detonate_STM/Uame-Purple_detonate_B1.bam.sorted.bam > detonate_STM/Uame-Purple_detonate_B1.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-Purple_detonate_B1.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_STM/Uame-Purple_detonate_B2.bam.sorted.bam > detonate_STM/Uame-Purple_detonate_B2.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-Purple_detonate_B2.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_STM/Uame-Purple_detonate_B3.bam.sorted.bam > detonate_STM/Uame-Purple_detonate_B3.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-Purple_detonate_B3.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_STM/Uame-White_detonate_B1.bam.sorted.bam > detonate_STM/Uame-White_detonate_B1.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-White_detonate_B1.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_STM/Uame-White_detonate_B2.bam.sorted.bam > detonate_STM/Uame-White_detonate_B2.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-White_detonate_B2.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_STM/Uame-White_detonate_B3.bam.sorted.bam > detonate_STM/Uame-White_detonate_B3.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-White_detonate_B3.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_STM/Uame-Yellow_detonate_B1.bam.sorted.bam > detonate_STM/Uame-Yellow_detonate_B1.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-Yellow_detonate_B1.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_STM/Uame-Yellow_detonate_B2.bam.sorted.bam > detonate_STM/Uame-Yellow_detonate_B2.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Uame-Yellow_detonate_B2.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_STM/Utri-Yellow_detonate_B1.bam.sorted.bam > detonate_STM/Utri-Yellow_detonate_B1.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_STM/Utri-Yellow_detonate_B1.bam_Q10.sorted.sam
+
+
+#samtools sort -n detonate_Trinity/Uame-Purple_detonate_B1.bam detonate_Trinity/Uame-Purple_detonate_B1.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-Purple_detonate_B1.bam.sorted.bam
+
+#samtools sort -n detonate_Trinity/Uame-Purple_detonate_B2.bam detonate_Trinity/Uame-Purple_detonate_B2.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-Purple_detonate_B2.bam.sorted.bam
+
+#samtools sort -n detonate_Trinity/Uame-Purple_detonate_B3.bam detonate_Trinity/Uame-Purple_detonate_B3.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-Purple_detonate_B3.bam.sorted.bam
+
+samtools sort -n detonate_Trinity/Uame-White_detonate_trinity_B1.bam detonate_Trinity/Uame-White_detonate_B1.bam.sorted
+SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-White_detonate_B1.bam.sorted.bam
+
+samtools sort -n detonate_Trinity/Uame-White_detonate_trinity_B2.bam detonate_Trinity/Uame-White_detonate_B2.bam.sorted
+SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-White_detonate_B2.bam.sorted.bam
+
+samtools sort -n detonate_Trinity/Uame-White_detonate_trinity_B3.bam detonate_Trinity/Uame-White_detonate_B3.bam.sorted
+SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-White_detonate_B3.bam.sorted.bam
+
+#samtools sort -n detonate_Trinity/Uame-Yellow_detonate_B1.bam detonate_Trinity/Uame-Yellow_detonate_B1.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-Yellow_detonate_B1.bam.sorted.bam
+
+#samtools sort -n detonate_Trinity/Uame-Yellow_detonate_B2.bam detonate_Trinity/Uame-Yellow_detonate_B2.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-Yellow_detonate_B2.bam.sorted.bam
+
+#samtools sort -n detonate_Trinity/Utri-Yellow_detonate_B1.bam detonate_Trinity/Utri-Yellow_detonate_B1.bam.sorted
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Utri-Yellow_detonate_B1.bam.sorted.bam
+
+#samtools view -q 10 detonate_Trinity/Uame-Purple_detonate_B1.bam.sorted.bam > detonate_Trinity/Uame-Purple_detonate_B1.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-Purple_detonate_B1.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_Trinity/Uame-Purple_detonate_B2.bam.sorted.bam > detonate_Trinity/Uame-Purple_detonate_B2.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-Purple_detonate_B2.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_Trinity/Uame-Purple_detonate_B3.bam.sorted.bam > detonate_Trinity/Uame-Purple_detonate_B3.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-Purple_detonate_B3.bam_Q10.sorted.sam
+
+samtools view -q 10 detonate_Trinity/Uame-White_detonate_B1.bam.sorted.bam > detonate_Trinity/Uame-White_detonate_B1.bam_Q10.sorted.sam
+SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-White_detonate_B1.bam_Q10.sorted.sam
+
+samtools view -q 10 detonate_Trinity/Uame-White_detonate_B2.bam.sorted.bam > detonate_Trinity/Uame-White_detonate_B2.bam_Q10.sorted.sam
+SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-White_detonate_B2.bam_Q10.sorted.sam
+
+samtools view -q 10 detonate_Trinity/Uame-White_detonate_B3.bam.sorted.bam > detonate_Trinity/Uame-White_detonate_B3.bam_Q10.sorted.sam
+SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-White_detonate_B3.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_Trinity/Uame-Yellow_detonate_B1.bam.sorted.bam > detonate_Trinity/Uame-Yellow_detonate_B1.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-Yellow_detonate_B1.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_Trinity/Uame-Yellow_detonate_B2.bam.sorted.bam > detonate_Trinity/Uame-Yellow_detonate_B2.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Uame-Yellow_detonate_B2.bam_Q10.sorted.sam
+
+#samtools view -q 10 detonate_Trinity/Utri-Yellow_detonate_B1.bam.sorted.bam > detonate_Trinity/Utri-Yellow_detonate_B1.bam_Q10.sorted.sam
+#SAM_nameSorted_to_uniq_count_stats.pl detonate_Trinity/Utri-Yellow_detonate_B1.bam_Q10.sorted.sam
+
+
+
+
