1e29dd55382f8824586ec9be569c4a3e80198bfe
SauraSilva <saura.silva@gmail.com>
2017-05-17 10:22:54 -0300

    Blastx search with non redundant database against processed reads
    Blastx search with protein database (non redundant database; NR) against post processed reads.

---
+++ Scripts/diamond.NRxUtric_reads.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+/home/srsilva/./diamond blastx -p 10 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-Purple_B1.scythe.cutadapt.prinseq_1.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-Purple_B1_prinseq_1
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-Purple_B1.scythe.cutadapt.prinseq_2.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-Purple_B1_prinseq_2
+
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-Purple_B2.scythe.cutadapt.prinseq_1.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-Purple_B2_prinseq_1
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-Purple_B2.scythe.cutadapt.prinseq_2.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-Purple_B2_prinseq_2
+
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-Purple_B3.scythe.cutadapt.prinseq_1.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-Purple_B3_prinseq_1
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-Purple_B3.scythe.cutadapt.prinseq_2.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-Purple_B3_prinseq_2
+
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-White_B1.scythe.cutadapt.prinseq_1.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-White_B1_prinseq_1
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-White_B1.scythe.cutadapt.prinseq_2.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-White_B1_prinseq_2
+
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-White_B2.scythe.cutadapt.prinseq_1.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-White_B2_prinseq_1
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-White_B2.scythe.cutadapt.prinseq_2.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-White_B2_prinseq_2
+
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-White_B3.scythe.cutadapt.prinseq_1.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-White_B3_prinseq_1
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-White_B3.scythe.cutadapt.prinseq_2.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-White_B3_prinseq_2
+
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-Yellow_B1.scythe.cutadapt.prinseq_1.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-Yellow_B1_prinseq_1
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-Yellow_B1.scythe.cutadapt.prinseq_2.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-Yellow_B1_prinseq_2
+
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-Yellow_B2.scythe.cutadapt.prinseq_1.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-Yellow_B2_prinseq_1
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Uame-Yellow_B2.scythe.cutadapt.prinseq_2.fastq -d ../ncbi_nr.dmnd -o 2nrxUame-Yellow_B2_prinseq_2
+
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Utri-Yellow_B1.scythe.cutadapt.prinseq_1.fastq -d ../ncbi_nr.dmnd -o 2nrxUtri-Yellow_B1_prinseq_1
+
+/home/srsilva/./diamond blastx -p 10 -f 101 --more-sensitive --query-cover 80 --evalue 0.001 --id 80 -q ../processed/prinseq/Utri-Yellow_B1.scythe.cutadapt.prinseq_2.fastq -d ../ncbi_nr.dmnd -o 2nrxUtri-Yellow_B1_prinseq_2
+
