f8dc21e16deb28bf2bcd0c08d611d5aef5d5efe5
SauraSilva <saura.silva@gmail.com>
2017-05-17 09:48:44 -0300

    Obtaining BUSCO quality results for Final Assembly
    Script to obtaining Benchmarking Universal Single-Copy Orthologs (BUSCO) quality results for Final Assembly

---
+++ Scripts/BUSCOxSTMCLUSTER.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+echo -e "\n Uame-Purple-STM-ClusterxBUSCO"
+BUSCO.py -c 10 -i ../Uame-Purple-2dez2016-selected.renamed.fa -o Uame-Purple-STM-Cluster -l ../../refs/BUSCO/embryophyta_odb9 -m tran
+
+#echo -e "\n Uame-White-STM-ClusterxBUSCO"
+#BUSCO.py -c 10 -i ../Uame-White-2dez2016-selected.renamed.fa -o Uame-White-STM-Cluster -l ../../refs/BUSCO/embryophyta_odb9 -m tran
+
+
+#echo -e "\n Uame-Yellow-STM-ClusterxBUSCO"
+
+#BUSCO.py -c 10 -i ../Uame-Yellow-2dez2016-selected.renamed.fa -o Uame-Yellow-STM-Cluster -l ../../refs/BUSCO/embryophyta_odb9 -m tran
+
+
+#echo -e "\n Utri-Yellow-STM-ClusterxBUSCO"
+
+#BUSCO.py -c 10 -i ../Utri-Yellow-2dez2016-selected.renamed.fa -o Utri-Yellow-STM-Cluster -l ../../refs/BUSCO/embryophyta_odb9 -m tran
+
+
+
