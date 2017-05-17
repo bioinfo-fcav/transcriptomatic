c29e1a8aeeb51cc78e8ef0de5529cf041f0841e2
SauraSilva <saura.silva@gmail.com>
2017-05-17 10:17:44 -0300

    Alignment with Final Assembly and specific KEGG Pathways
    Alignment with Final Assembly and specific KEGG Pathways

---
+++ Scripts/assmxko00941.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+/data/project/flowers/./diamond blastx -p 10 -f 6 --more-sensitive --query-cover 60 --evalue 0.000001 --id 60 -q ../../readsxtranscriptomes/Uame-Purple-2dez2016-selected.renamed.fa -d ko00941.dmnd -o ko00941xUame-Purple_assm_id60_moresensitive_cov60_evalue10-6
+
+/data/project/flowers/./diamond blastx -p 10 -f 6 --more-sensitive --query-cover 60 --evalue 0.000001 --id 60 -q ../../readsxtranscriptomes/Uame-White-2dez2016-selected.renamed.fa -d ko00941.dmnd -o ko00941xUame-White_assm_id60_moresensitive_cov60_evalue10-6
+
+/data/project/flowers/./diamond blastx -p 10 -f 6 --more-sensitive --query-cover 60 --evalue 0.000001 --id 60 -q ../../readsxtranscriptomes/Uame-Yellow-2dez2016-selected.renamed.fa -d ko00941.dmnd -o ko00941xUame-Yellow_assm_id60_moresensitive_cov60_evalue10-6
+
+/data/project/flowers/./diamond blastx -p 10 -f 6 --more-sensitive --query-cover 60 --evalue 0.000001 --id 60 -q ../../readsxtranscriptomes/Utri-Yellow-2dez2016-selected.renamed.fa -d ko00941.dmnd -o ko00941xUtri-Yellow_assm_id60_moresensitive_cov60_evalue10-6
+
+
